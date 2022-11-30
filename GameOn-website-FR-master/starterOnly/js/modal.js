78fa88ee8f64e7e0f0237ba0115502c379217a05
neochatelin <neo.chatelin@gmail.com>
2022-11-30 23:56:10 +0100

    Ajouter validation ou messages d'erreur

--- GameOn-website-FR-master/starterOnly/js/modal.js
+++ GameOn-website-FR-master/starterOnly/js/modal.js
@@ -16,6 +16,8 @@ const formData = document.querySelectorAll(".formData");
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
 
 // launch modal form
+let data;
+
 function launchModal() {
   if(window.localStorage.getItem("modalForm")){
     let modalForm = JSON.parse(window.localStorage.getItem("modalForm"));
@@ -34,6 +36,7 @@ function launchModal() {
     formData[6].children['checkbox2'].checked = modalForm.checkbox2;
   }
   modalbg.style.display = "block";
+  checking();
 }
 
 // close modal form
@@ -41,25 +44,66 @@ let closeModal=()=>{
   modalbg.style.display = "none";
 }
 
-// saving form in local storage
-let data;
-
-let saving=()=>{
+// saving form data in local storage
+let saveFormData=()=>{
   data = {
     first: formData[0].children[2].value,
     last: formData[1].children[2].value,
     email: formData[2].children[2].value,
     birthdate: formData[3].children[2].value,
     quantity: formData[4].children[2].value,
-    location: '',
+    location: Array.from(formData[5].children).forEach((e)=>{
+                if(e.type === "radio" && e.checked)
+                  return e.value;
+              }),
     checkbox1: formData[6].children['checkbox1'].checked,
     checkbox2: formData[6].children['checkbox2'].checked
   }
-  Array.from(formData[5].children).forEach((e)=>{
-    if(e.type === "radio" && e.checked)
-      data.location = e.value;
-  });
+  
   window.localStorage.setItem("modalForm", JSON.stringify(data));
+}
+
+// checking
+let checking = (e)=>{
+  saveFormData();
+  let btn = document.getElementsByClassName('btn-submit')[0];
+  let isCorrect = true;
+  switch (e) {
+    case "first":
+      (data.first).length >= 2 ?
+        document.querySelector('.error-msg-first').style.display = "none"
+        :
+        document.querySelector('.error-msg-first').style.display = "block"
+      break;
+    case "last":
+      data.last.length >= 2 ?
+        document.querySelector('.error-msg-last').style.display = "none"
+        :
+        document.querySelector('.error-msg-last').style.display = "block"
+      break;
+    case "email":
+      (data.email).match(/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g)?
+        document.querySelector('.error-msg-email').style.display = "none"
+        :
+        document.querySelector('.error-msg-email').style.display = "block"
+      break;
+    case "birthdate":
+      data.value != "" ?
+        document.querySelector('.error-msg-first').style.display = "none"
+        :
+        document.querySelector('.error-msg-first').style.display = "block"
+      break;
+    case "quantity":
+      data.quantity < 0 && typeof parseInt(data.quantity) !== "number" ?isCorrect=false:'';
+      break;
+    case "checkbox":
+      data.checkbox1?'':isCorrect=false;
+      break;
+
+    default:
+      break;
+  }
+  isCorrect? btn.style.background = '#fe142f' : btn.style.background = 'grey';
 }
 
 // validate modal form
